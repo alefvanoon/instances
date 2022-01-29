3ef3c10012354405205ff53f2dfc34b83aa5cd60
ManeraKai <manerakai@protonmail.com>
2022-01-29 13:01:44 +0300

    removed http protocols

--- get_instances.py
+++ get_instances.py
@@ -9,10 +9,7 @@ r = requests.get('https://api.invidious.io/instances.json')
 rJson = json.loads(r.text)
 invidiousList = []
 for instance in rJson:
-    if instance[0].endswith(".onion"):
-        invidiousList.append("http://"+instance[0])
-    else:
-        invidiousList.append("https://"+instance[0])
+    invidiousList.append(instance[0])
 mightyList["youtube"] = invidiousList
 
 
@@ -30,10 +27,7 @@ for table in tables:
     for tr in trs:
         td = tr.find("td")
         a = td.find("a")
-        if a.contents[0].endswith(".onion"):
-            nitterList.append("http://"+a.contents[0])
-        else:
-            nitterList.append("https://"+a.contents[0])
+        nitterList.append(a.contents[0])
 mightyList["twitter"] = nitterList
 
 
@@ -42,7 +36,10 @@ r = requests.get('https://bibliogram.art/api/instances')
 rJson = json.loads(r.text)
 bibliogramList = []
 for instance in rJson["data"]:
-    bibliogramList.append(instance["address"])
+    result = instance["address"]
+    result = result.replace("https://", "")
+    result = result.replace("http://", "")
+    bibliogramList.append(result)
 mightyList["instagram"] = bibliogramList
 
 
